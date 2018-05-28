333c0cd8c579170945593ec86c988c81c9656604
Antonius97 <aantonn1997@yandex.ru>
2018-05-28 15:05:06 +0300

    Base template build

---
+++ docs/precache-manifest.235cb90de68c08ccc370ce2164affffb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e29ab9bb508e10a3852",
+    "url": "/js/vendors~app.5e29ab9b.js"
+  },
+  {
+    "revision": "4ee393e58aa89d1a5e5c",
+    "url": "/js/app.4ee393e5.js"
+  },
+  {
+    "revision": "6dfc3e63dd07ed601fa56e675979ad3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ee393e58aa89d1a5e5c",
+    "url": "/css/app.ec03bac3.css"
+  }
+];
